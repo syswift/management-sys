03ce2083dffe7266f5e535e60f71abcbaab22567
syswift <myworlddai97@gmail.com>
2022-08-06 00:03:13 +1200

    try this

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -4,7 +4,7 @@ const buildClient = ({ req }) => {
     if(typeof window === 'undefined'){
         // we are on the server
         return axios.create({
-            baseURL: 'http://www.syswift.me/',
+            baseURL: 'syswift.me',
             headers: req.headers
         });
     }else{
